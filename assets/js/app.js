c3624c0d97792d7eb8ef338c51ef82b1b9f487cb
Ryan Richholt <rrichholt@tgen.org>
2017-09-09 13:43:29 -0700

    modified some of the brewerydb functions, added firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,84 +1,77 @@
 // Code for brewery ranking website
 
-function getBreweries(params){
-    // Logs info to console
-    var apiKey = "70ec47e3e10b786dfce3d08410c16454";
-    var URL = "https://api.brewerydb.com/v2/breweries/";
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBtqVuU6Qtl_OtteNN2VYcS32PkzvT3VRo",
+    authDomain: "myawesomeproject-131ac.firebaseapp.com",
+    databaseURL: "https://myawesomeproject-131ac.firebaseio.com",
+    projectId: "myawesomeproject-131ac",
+    storageBucket: "myawesomeproject-131ac.appspot.com",
+    messagingSenderId: "369533829285"
+};
+firebase.initializeApp(config);
 
-    par = {
-        key: apiKey,
-        format: 'json',
-    };
+var database = firebase.database()
 
-    $.extend(true, par, params)
 
-    $.ajax({
-        url: URL,
-        method: "GET",
-        data: $.param(par)
-    }).done(function(response) {
-        console.log(response)
-        console.log(response.data)
-    });
+database.ref().once("value").then(function(snapshot){
+    var data = snapshot.val()
+    for(i in data){
+        build_brewery_button(data[i])
+    }
+})
+
+
+function scan_cities(){
+    var cities = ['Tempe', 'Scottsdale', 'Phoenix', 'Mesa']
+    for(i in cities){
+        findBreweriesInCity(cities[i])
+    }
 }
 
 
-function getBreweriesLocations(params){
-    // Logs info to console
+function findBreweriesInCity(city){
     var apiKey = "70ec47e3e10b786dfce3d08410c16454";
     var URL = "https://api.brewerydb.com/v2/locations/";
     
     par = {
         key: apiKey,
         format: 'json',
+        locality: city,
     };
-
-    $.extend(true, par, params)
 
     $.ajax({
         url: URL,
         method: "GET",
         data: $.param(par)
     }).done(function(response) {
-        console.log(response)
-        console.log(response.data)
+        for(i in response.data){
+            var brewery = response.data[i]
+            console.log(brewery)
+            console.log(brewery.id)
+            var db_obj = database.ref().child(brewery.id)
+            db_obj.update(brewery)
+        }
     });
 }
 
 
-function get_breweries_ranked(n=10){
-    // Ryan finishes this function
-    var breweries = [
-        {
-            name: 'ohso',
-           // logo_url: http://url.for.logo
-           // location: Arcadia, AZ
-           // price: $$
-           // beers: [Orange Blossom, Stawberry Blonde]
-           // phone number: 480-123-4567
-        },
-        {
-            name: 'fourpeaks',
-        },
-        {
-            name: 'helton',
-        },
-        {
-            name: 'motherbunch'
-        }
-    ]
-    return breweries
-}
+function upvote_brewery(breweryId){
+    var voted = localStorage.getItem('voted');
+    if(!voted){
+        localStorage.setItem('voted', true)
+        var databaseRef = database.ref(breweryId).child('votes')
+        console.log(databaseRef)
+        databaseRef.transaction(function(votes) {
+            if (votes) {
+                votes = votes + 1;
+            } else {
+                votes = 1
+            }
 
-
-// Still needed
-function get_yelp_info(brewery_name){
-    // gets all the data we need from yelp
-}
-
-
-function get_brewerydb_info(brewery_name){
-    // gets all the data we need from brewerydb
+            return votes;
+        });
+    }
 }
 
 
@@ -86,6 +79,8 @@ function build_brewery_button(brewery_obj){
     // given a brewery object, like the example in get_beweries_ranked(), 
     // return a new html element including the expandable div and all the info
     // from the brewery_obj
+    console.log('building button for ' + brewery_obj.brewery.name)
+    console.log(brewery_obj)
 }
 
 
